7743b448025717a11f9b37091963def6286df446
JIALUN LIU <jl4347@columbia.edu>
2015-04-18 17:04:14 -0400

    Create a dict to store the violation code of the restaurant

--- process.py
+++ process.py
@@ -8,7 +8,9 @@ def parseCSV(filename):
 		read from CSV file, then process it in whatever way we want
 	'''
 	firstRow = True
-	data = list()
+	restaurant = dict()
+	count = 0
+	# violationCode = dict()
 	with open(filename, 'r') as csvfile:
 		spamreader = csv.reader(csvfile.read().splitlines())
 		for row in spamreader:
@@ -19,43 +21,19 @@ def parseCSV(filename):
 				# print row[23], row[24], row[25]
 				firstRow = False
 			else:
-				if row[10] == 'Annual':
-					# round annual salary to the nearest 10000
-					row[8] = math.floor(int(row[8])/10000)*10000
-					row[9] = math.ceil(int(row[9])/10000)*10000
-				elif row[10] == 'Hourly':
-					# round hourly salary to the nearest 5
-					row[8] = math.floor(int(row[8])/5)*5
-					row[9] = math.ceil(int(row[9])/5)*5
+				if row[1] != '':
+					if row[1] not in restaurant:
+						count += 1
+						restaurant[row[1]] = set()
+					if row[2] != '':
+						restaurant[row[1]].add(row[2])
 
-				# Posting Date only have month and year
-				# row[22] = row[22][0:2] + row[22][6:9]
-				# print row[22]
-				
-				# delete the Post until, Posting update and Process date dimension
-				# print row[23], row[24], row[25]
-				# del row[25]
-				# del row[24]
-				# del row[23]
-
-				# delete the Recrument contact
-				# del row[20]
-
-				# combine the salary range from and range to fields
-				# row.append(str(row[8]) + '-' + str(row[9]))
-				row[8] = 'f' + str(int(row[8]))
-				row[9] = 't' + str(int(row[9]))
-				# print row[11]
-				m = re.match("(\d+\/)\d+\/(\d+) \d+", str(row[11]))
-				row.append(m.group(1) + m.group(2))
-				del row[11]
-				# del Num of position dimension
-				del row[3]
-				# del row[9]
-				# del row[8]
-				data.append(row)
+				# if row[2] not in violationCode:
+				# 	violationCode[row[2]] = row[]
+	print restaurant
+	print count
 	
-	return data
+	return restaurant
 
 def writeCSV(data, fileName):
 	'''
